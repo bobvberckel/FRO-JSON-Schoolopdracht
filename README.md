fd55cbcb8f79832299c464483b0f6f21b76a36e0
Bob van Berckel <43894787+bobvberckel@users.noreply.github.com>
2020-11-27 12:37:13 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# FRO JSON - Schoolopdracht
-
+# FRO - JSON Importeren.
 [Link naar live version](http://29198.hosts2.ma-cloud.nl/bewijzenmap/periode2.1/fro/fro_json_boeken/)
+
+# Disclaimer
+De code is geheel naar eigen voorkeur herschreven. Dit is terug te zien in de comments en de eigen backticks die gebruikt zijn in de code.
--- assets/scripts/boeken_xhr.js
+++ assets/scripts/boeken_xhr.js
@@ -1,95 +1,112 @@
-// HTML ID ophalen.
-const output = document.getElementById(`boeken`);
-
-// XMLHttpRequest aanmaken.
-const xhr = new XMLHttpRequest();
-
-// Checkboxen voor de taalfilter.
-const taalKeuze = document.querySelectorAll(`.besturing__cb-taal`);
+const output = document.getElementById(`boeken`); // HTML ID ophalen.
+const xhr = new XMLHttpRequest(); // XMLHttpRequest aanmaken.
+const taalKeuze = document.querySelectorAll(`.besturing__cb-taal`); // Checkboxen voor de taalfilter ophalen uit HTML.
+const selectKeuze = document.querySelector(`.besturing__select`); // Input voor de dropdown ophalen uit HTML.
 
 // XMLHttpRequest instellen.
 xhr.onreadystatechange = () => {
+    // XMLHttpRequest gegevens afwachten.
     if (xhr.readyState == 4 && xhr.status == 200) {
+        // Resultaat opslaan.
         let xhrResult = JSON.parse(xhr.responseText);
 
-        boeken.filteren(xhrResult);
-        boeken.uitvoeren();
+        boeken.filteren(xhrResult); // Boeken worden eerst gefilterd.
+        boeken.uitvoeren(); // Boeken worden na het filteren pas uitgevoerd (Richting de HTML gestuurd).
     }
 }
 
-// XMLHttpRequest open zetten en informatie meegeven.
-xhr.open(`GET`, `./assets/json/boeken.json`, true);
-
-// XMLHttpRequest versturen.
-xhr.send();
+xhr.open(`GET`, `./assets/json/boeken.json`, true); // XMLHttpRequest open zetten en informatie meegeven.
+xhr.send(); // XMLHttpRequest versturen.
 
 const boeken = {
-    // Taal filter bepalen.
-    taalFilter: ["Engels", "Duits", "Nederlands"],
+        taalFilter: ["Engels", "Duits", "Nederlands"], // Taalfilter bepalen.
+        eigenschapSorteren: 'titel', // Eigenschapfilter bepalen.
+        oplopend: 1, // Oplopende functie activeren of deactiveren.
 
-    // Filter uitvoeren op talen.
-    filteren(gegevens) {
-        this.data = gegevens.filter((bk) => {
-            let bool = false;
-            this.taalFilter.forEach((taal) => {
-                if (bk.taal == taal) {
-                    bool = true;
-                }
+        // Filter uitvoeren op talen.
+        filteren(gegevens) {
+            this.data = gegevens.filter((bk) => {
+                // Filter proces.
+                let bool = false; // Standaard value instellen.
+                this.taalFilter.forEach((taal) => { // Door elke boektaal loopen.
+                    if (bk.taal == taal) {
+                        bool = true;
+                    }
+                });
+
+                return bool; // Return result.
             });
+        },
 
-            return bool;
-        });
-    },
-
-    uitvoeren() {
-        let html = "";
-        this.data.forEach(boek => {
-            let completeTitel = "";
-            if (boek.voortitel) {
-                completeTitel += boek.voortitel + " ";
+        // Sorteer functie.
+        sorteren() {
+            if (this.eigenschapSorteren == `titel`) { // Sorteren op titel.
+                this.data.sort((a, b) => (a.titel.toUpperCase() > b.titel.toUpperCase()) ? this.oplopend : -1 * this.oplopend);
+            } else if (this.eigenschapSorteren == `paginas`) { // Sorteren op pagina's.
+                this.data.sort((a, b) => (a.paginas > b.paginas) ? this.oplopend : -1 * this.oplopend);
+            } else if (this.eigenschapSorteren == `uitgave`) { // Sorteren op uitgave.
+                this.data.sort((a, b) => (a.uitgave > b.uitgave) ? this.oplopend : -1 * this.oplopend);
+            } else if (this.eigenschapSorteren == `prijs`) { // Sorteren op prijs.
+                this.data.sort((a, b) => (a.prijs > b.prijs) ? this.oplopend : -1 * this.oplopend);
+            } else if (this.eigenschapSorteren == `auteur`) {
+                this.data.sort((a, b) => (a.auteurs[0].achternaam.toUpperCase() > b.auteurs[0].achternaam.toUpperCase()) ? this.oplopend : -1 * this.oplopend); // Sorteren op auteur.
             }
-            completeTitel += boek.titel;
+        },
 
-            // Auteur lijstje aanmaken.
-            let auteurs = "";
-            boek.auteurs.forEach((schrijver, index) => {
-                let tussenvoegsel = schrijver.tussenvoegsel ? schrijver.tussenvoegsel + " " : "";
-                let separator = ", ";
+        // Startup functie.
+        uitvoeren() {
+            this.sorteren(); // Beginnen met sorteren.
 
-                if (index >= boek.auteurs.length - 2) {
-                    separator = " en ";
-                }
+            let html = "";
+            this.data.forEach(boek => {
+                        let completeTitel = "";
+                        if (boek.voortitel) { // Als een boek een voortitel heeft, wordt deze vóór de originele titel geplaatst.
+                            completeTitel += boek.voortitel + " ";
+                        }
+                        completeTitel += boek.titel;
 
-                if (index >= boek.auteurs.length - 1) {
-                    separator = "";
-                }
+                        // Auteur lijstje aanmaken.
+                        let auteurs = "";
+                        boek.auteurs.forEach((schrijver, index) => {
+                            let tussenvoegsel = schrijver.tussenvoegsel ? schrijver.tussenvoegsel + " " : ""; // Kijken of er een tussenvoegsel is, zo ja wordt deze toegevoegd.
+                            let separator = ", "; // Mochten er meerdere schrijvers zijn, worden die schrijvers geseperateerd met een komma.
 
-                auteurs += schrijver.voornaam + " " + tussenvoegsel + schrijver.achternaam + separator;
-            });
+                            if (index >= boek.auteurs.length - 2) {
+                                separator = " en "; // Als het de laatste schrijver is, worden die schrijvers geseperateerd met een "en".
+                            }
 
-            // HTML samenstellen.
-            html += `<section class="boek">`;
-            html += `<img class="boek__cover" src="${boek.cover}" alt="${completeTitel}">`
-            html += `<h3 class="boek__kopje">${completeTitel}</h3>`;
-            html += `<p class="boek__auteurs">${auteurs}</p>`
-            html += `<span class="boek__uitgave">Uitgave: ${this.datumOmzetten(boek.uitgave)}</span>`;
-            html += `<span class="boek__ean">EAN: ${boek.ean}</span>`;
-            html += `<span class="boek__paginas">Aantal bladzijdes: ${boek.paginas}</span>`;
-            html += `<span class="boek__taal">Taal: ${boek.taal}</span>`;
-            html += `<div class="boek__prijs">Prijs: ${boek.prijs.toLocaleString(`nl-NL`, {currency: 'EUR', style: 'currency'})}</div>`;
+                            if (index >= boek.auteurs.length - 1) {
+                                separator = ""; // Default separator.
+                            }
+
+                            auteurs += schrijver.voornaam + " " + tussenvoegsel + schrijver.achternaam + separator; // Auteurs mét voortitel, tussenvoegsel, achternaam en separator toevoegen.
+                        });
+
+                        // HTML samenstellen.
+                        html += `<section class="boek">`;
+                        html += `<img class="boek__cover" src="${boek.cover}" alt="${completeTitel}">`
+                        html += `<h3 class="boek__kopje">${completeTitel}</h3>`;
+                        html += `<p class="boek__auteurs">${auteurs}</p>`
+                        html += `<span class="boek__uitgave">Uitgave: ${this.datumOmzetten(boek.uitgave)}</span>`;
+                        html += `<span class="boek__ean">EAN: ${boek.ean}</span>`;
+                        html += `<span class="boek__paginas">Aantal bladzijdes: ${boek.paginas}</span>`;
+                        html += `<span class="boek__taal">Taal: ${boek.taal}</span>`;
+                        html += `<div class="boek__prijs">Prijs: ${boek.prijs.toLocaleString(`nl-NL`, {currency: 'EUR', style: 'currency'})}</div>`;
             html += `</section>`;
         });
-        output.innerHTML = html;
+        output.innerHTML = html; // Output tonen op de website.
     },
 
+    // Datum in de juiste formaat zetten.
     datumOmzetten(datumString) {
-        let datum = new Date(datumString);
-        let jaar = datum.getFullYear();
-        let maand = this.geefMaandnaam(datum.getMonth());
+        let datum = new Date(datumString); // Datum variabele aanmaken.
+        let jaar = datum.getFullYear(); // Jaar ophalen uit datum variabele.
+        let maand = this.geefMaandnaam(datum.getMonth()); // Maand ophalen  uit datum variabele door middel van de JSON data.
 
-        return `${maand} ${jaar}`;
+        return `${maand} ${jaar}`; // Data terug sturen.
     },
 
+    // Maand nummer omzetten naar een maand naam.
     geefMaandnaam(m) {
         let maand = "";
         switch (m) {
@@ -132,19 +149,34 @@ const boeken = {
             default:
                 maand = m;
         }
-        return maand;
+        return maand; // Data terug sturen.
     }
 }
 
+// Filter aanpassen naar gebruiker voorkeur.
 const pasFilterAan = () => {
-    let gecheckteTaalKeuze = [];
-    taalKeuze.forEach(cb => {
-        if (cb.checked) gecheckteTaalKeuze.push(cb.value);
+    let gecheckteTaalKeuze = []; // Array aanmaken voor de gekozen talen.
+    taalKeuze.forEach(cb => { // Lopen door de gekozen taalkeuzes.
+        if (cb.checked) gecheckteTaalKeuze.push(cb.value); // Gekozen taalkeuzes in de array zetten.
     });
 
-    boeken.taalFilter = gecheckteTaalKeuze;
-    boeken.filteren(JSON.parse(xhr.responseText));
-    boeken.uitvoeren();
+    boeken.taalFilter = gecheckteTaalKeuze; // Array wegpushen naar uitvoer functie.
+    boeken.filteren(JSON.parse(xhr.responseText)); // Array in JS taal omzetten.
+    boeken.uitvoeren(); // Uitvoer functie opnieuw uitvoeren.
+}
+
+// Sorteren op eigenschappen gekozen door de gebruiker.
+const pasSortEigenschapAan = () => {
+    boeken.eigenschapSorteren = selectKeuze.value; // Gekozen eigenschappen doorsturen naar uitvoer functie.
+    boeken.uitvoeren(); // Uitvoer functie opnieuw uitvoeren.
 }
 
 taalKeuze.forEach(cb => cb.addEventListener(`change`, pasFilterAan));
+
+selectKeuze.addEventListener(`change`, pasSortEigenschapAan);
+
+// Oplopend / Aflopend functie uitvoeren. 
+document.querySelectorAll(`.besturing__rb`).forEach(rb => rb.addEventListener(`change`, () => {
+    boeken.oplopend = rb.value; // Eigenschap doorsturen naar uitvoer functie.
+    boeken.uitvoeren(); // Uitvoer functie opnieuw uitvoeren.
+}));
